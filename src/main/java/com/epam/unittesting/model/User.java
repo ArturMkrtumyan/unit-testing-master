1d6f9a8e08b081157f86c37c526f56f2a1b1cd90
Admin <mkrtart9211@gmail.com>
2021-12-24 19:05:14 +0400

    Initial commit

---
+++ src/main/java/com/epam/unittesting/model/User.java
@@ -0,0 +1,36 @@
+package com.epam.unittesting.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String surname;
+
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+
+}
